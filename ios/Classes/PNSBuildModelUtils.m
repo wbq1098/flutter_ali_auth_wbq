d311ff085a3132f1772e7a71037708cdfe0e15cb
wbq1098 <wbq1098@163.com>
2020-10-04 14:48:36 +0800

    解决ios界面展示问题

--- ios/Classes/PNSBuildModelUtils.m
+++ ios/Classes/PNSBuildModelUtils.m
@@ -40,18 +40,18 @@
     model.privacyNavTitleFont = [UIFont systemFontOfSize:20.0];
     model.privacyNavTitleColor = UIColor.whiteColor;
     
-    //model.logoImage = [UIImage imageNamed:@"logo"];
-    //model.logoIsHidden = NO;
+    model.logoImage = [UIImage imageNamed:@"logo"];
+    model.logoIsHidden = NO;
     //model.sloganIsHidden = NO;
     // model.sloganText = [[NSAttributedString alloc] initWithString:@"一键登录slogan文案" attributes:@{NSForegroundColorAttributeName : UIColor.orangeColor,NSFontAttributeName : [UIFont systemFontOfSize:16.0]}];
     model.numberColor = [self colorWithHex:0xFFA346 alpha: 1];
     model.numberFont = [UIFont systemFontOfSize:30.0];
     model.loginBtnText = [[NSAttributedString alloc] initWithString:@"一键登录" attributes:@{NSForegroundColorAttributeName : UIColor.whiteColor,NSFontAttributeName : [UIFont systemFontOfSize:20.0]}];
-    //model.loginBtnBgImgs = @[
-    //  [UIImage imageNamed:@"button"],
-    //  [UIImage imageNamed:@"button"],
-    //  [UIImage imageNamed:@"button"]
-    //];
+    model.loginBtnBgImgs = @[
+      [UIImage imageNamed:@"button"],
+      [UIImage imageNamed:@"button"],
+      [UIImage imageNamed:@"button"]
+    ];
     //model.autoHideLoginLoading = NO;
     // model.privacyOne = @[@"《隐私1》",@"https://www.taobao.com/"];
     //model.privacyTwo = @[@"《隐私2》",@"https://www.taobao.com/"];
