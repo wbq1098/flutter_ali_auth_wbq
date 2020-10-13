6a7cde2566e130a43e1c2cfcffc8c64c162989df
wangbaoquan <wbq1098@163.com>
2020-10-13 14:41:30 +0800

    修改yaml文件，发布到pub

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'package:ali_auth_wbq/ali_auth.dart';
+import 'package:ali_auth_wbq/ali_auth_wbq.dart';
 import 'dart:io';
 
 void main() => runApp(MaterialApp(home: MyApp()));
